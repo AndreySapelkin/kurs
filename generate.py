c07af56da4e5be3877d97fff34a23fc2512cb0a4
AndreySapelkin <asapelkin86@yandex.ru>
2019-01-23 11:35:10 +0300

    Итоговый комит

--- generate.py
+++ generate.py
@@ -2,7 +2,8 @@
 from maze import *
 from random import *
 
-#вспомогательный класс
+
+# вспомогательный класс
 class Wall:
     def __init__(self):
         self.x = 0
@@ -11,7 +12,7 @@ class Wall:
         self.dy = 0
 
 
-#класс алгоритмов генерации лабиринтов
+# класс алгоритмов генерации лабиринтов
 class GenMaze:
 
     def __init__(self):
@@ -27,30 +28,30 @@ class GenMaze:
         else:
             maze.SetLocUpWall(x, y + 1, False)
 
-    #генерация лабиринта методом Прима
+    # генерация лабиринта методом Прима
     def GenPrime(self, maze):
 
         w = maze.GetWidth()
         h = maze.GetHeight()
 
-        #изначально все стены существуют
+        # изначально все стены существуют
         for i in range(w):
             for j in range(h):
                 maze.SetLoc(i, j, True, True)
 
-        #изначально все атрибуты - O (Outside)
-        AtribLocs = [[ "O" for x in range(h)] for y in range (w)]
+        # изначально все атрибуты - O (Outside)
+        AtribLocs = [["O" for x in range(h)] for y in range(w)]
 
-        #выбираем начальную локацию и присваиваем ей атрибут I (Inside)
-        x = randint(1,w-1);
-        y = randint(1,h-1);
+        # выбираем начальную локацию и присваиваем ей атрибут I (Inside)
+        x = randint(1, w - 1);
+        y = randint(1, h - 1);
         AtribLocs[x][y] = "I";
 
-        #локациям, граничным с начальной, присваиваем атрибут B (Border)
+        # локациям, граничным с начальной, присваиваем атрибут B (Border)
         xc = 0
         yc = 0
-        dx = [ 1, 0, -1, 0 ]
-        dy = [ 0, -1, 0, 1 ]
+        dx = [1, 0, -1, 0]
+        dy = [0, -1, 0, 1]
         for i in range(4):
             xc = x + dx[i]
             yc = y + dy[i]
@@ -74,12 +75,12 @@ class GenMaze:
 
             for i in range(w):
                 for j in range(h):
-                     if (AtribLocs[i][j] == "B"):
-                         counter -= 1
-                         if (counter == 0):
-                                xloc = i
-                                yloc = j
-                                break
+                    if (AtribLocs[i][j] == "B"):
+                        counter -= 1
+                        if (counter == 0):
+                            xloc = i
+                            yloc = j
+                            break
                 if (counter == 0):
                     break
 
@@ -96,7 +97,7 @@ class GenMaze:
                     if (AtribLocs[xc][yc] == "O"):
                         AtribLocs[xc][yc] = "B"
 
-            counter = randint(1,counter)
+            counter = randint(1, counter)
             for i in range(4):
                 xc = xloc + dx[i]
                 yc = yloc + dy[i]
@@ -108,16 +109,118 @@ class GenMaze:
 
             for i in range(w):
                 for j in range(h):
-                     if (AtribLocs[i][j] == "B"):
-                         IsEnd = False
-                         break
+                    if (AtribLocs[i][j] == "B"):
+                        IsEnd = False
+                        break
                 if (IsEnd == False):
                     break
-                 
 
         maze.PrintToConsole();
 
-    #можно ли пройти из локации (x,y) в локацию (x+dx,y+dy)
+    # пошаговая генерация лабиринта методом Прима
+    def GenPrimeSS(self, maze):
+
+        All = False;
+        Dlg = "";
+        w = maze.GetWidth()
+        h = maze.GetHeight()
+
+        # изначально все стены существуют
+        for i in range(w):
+            for j in range(h):
+                maze.SetLoc(i, j, True, True)
+
+        # изначально все атрибуты - O (Outside)
+        AtribLocs = [["O" for x in range(h)] for y in range(w)]
+
+        # выбираем начальную локацию и присваиваем ей атрибут I (Inside)
+        x = randint(1, w - 1);
+        y = randint(1, h - 1);
+        AtribLocs[x][y] = "I";
+
+        # локациям, граничным с начальной, присваиваем атрибут B (Border)
+        xc = 0
+        yc = 0
+        dx = [1, 0, -1, 0]
+        dy = [0, -1, 0, 1]
+        for i in range(4):
+            xc = x + dx[i]
+            yc = y + dy[i]
+            if ((xc >= 0) and (yc >= 0) and (xc < w) and (yc < h)):
+                AtribLocs[xc][yc] = "B";
+
+        IsEnd = False
+        counter = 0
+        xloc = 0
+        yloc = 0
+
+        while (IsEnd == False):
+            IsEnd = True
+            counter = 0
+            for i in range(w):
+                for j in range(h):
+                    if (AtribLocs[i][j] == "B"):
+                        counter += 1
+
+            counter = randint(1, counter)
+
+            for i in range(w):
+                for j in range(h):
+                    if (AtribLocs[i][j] == "B"):
+                        counter -= 1
+                        if (counter == 0):
+                            xloc = i
+                            yloc = j
+                            break
+                if (counter == 0):
+                    break
+
+            AtribLocs[xloc][yloc] = "I"
+
+            counter = 0;
+
+            for i in range(4):
+                xc = xloc + dx[i]
+                yc = yloc + dy[i]
+                if ((xc >= 0) and (yc >= 0) and (xc < w) and (yc < h)):
+                    if (AtribLocs[xc][yc] == "I"):
+                        counter += 1
+                    if (AtribLocs[xc][yc] == "O"):
+                        AtribLocs[xc][yc] = "B"
+
+            counter = randint(1, counter)
+            for i in range(4):
+                xc = xloc + dx[i]
+                yc = yloc + dy[i]
+                if ((xc >= 0) and (yc >= 0) and (xc < w) and (yc < h) and (AtribLocs[xc][yc] == "I")):
+                    counter -= 1;
+                    if (counter == 0):
+                        self.BreakWall(maze, xloc, yloc, dx[i], dy[i])
+                        if (All == False):
+                            maze.PrintToConsole()
+                            while (True):
+                                print("Нажмите <Enter> для перехода к следующему шагу генерации;")
+                                print(
+                                    "Введите 'q' - если хотите пропустить оставшиеся промежуточные ходы пошаговой генерации")
+                                Dlg = input()
+                                if (Dlg == ""):
+                                    break
+                                if (Dlg == "q"):
+                                    All = True
+                                    break
+                        break
+
+            for i in range(w):
+                for j in range(h):
+                    if (AtribLocs[i][j] == "B"):
+                        IsEnd = False
+                        break
+                if (IsEnd == False):
+                    break
+
+        maze.PrintToConsole();
+
+    # можно ли пройти из локации (x,y) в локацию (x+dx,y+dy)
     def CanGo(self, maze, x, y, dx, dy):
         if (dx == -1):
             return not maze.GetLoc(x, y).left_wall
@@ -128,22 +231,21 @@ class GenMaze:
         else:
             return not maze.GetLoc(x, y + 1).up_wall
 
-
-    #ответ на вопрос - существует ли путь между локациями (xs, ys) и (xf, yf) 
-    #методом волновой трассировки (необходмо для генерации лабиринта методом Краскала)
+    # ответ на вопрос - существует ли путь между локациями (xs, ys) и (xf, yf)
+    # методом волновой трассировки (необходмо для генерации лабиринта методом Краскала)
     def WaveTracingSolve(self, maze, xs, ys, xf, yf):
-        dx = [ 1, 0, -1, 0 ]
-        dy = [ 0, -1, 0, 1 ]
+        dx = [1, 0, -1, 0]
+        dy = [0, -1, 0, 1]
 
         N = 1;
 
         w = maze.GetWidth()
         h = maze.GetHeight()
 
-        Mark = [[ 0 for x in range(h)] for y in range (w)]
-        
+        Mark = [[0 for x in range(h)] for y in range(w)]
+
         Mark[xs][ys] = 1
-            
+
         no_sol = False
         while (no_sol == False):
             no_sol = True;
@@ -151,31 +253,31 @@ class GenMaze:
                 for j in range(h):
                     if (Mark[i][j] == N):
                         for k in range(4):
-                            if ((self.CanGo(maze, i, j, dx[k], dy[k])) and 
+                            if ((self.CanGo(maze, i, j, dx[k], dy[k])) and
                                     (Mark[i + dx[k]][j + dy[k]] == 0)):
                                 no_sol = False
                                 Mark[i + dx[k]][j + dy[k]] = N + 1
-                                if (((i + dx[k]) == xf) and ((j + dy[k])==yf)):
+                                if (((i + dx[k]) == xf) and ((j + dy[k]) == yf)):
                                     return True;
             N += 1
         return False;
 
-    #генерация лабиринта методом Краскала
+    # генерация лабиринта методом Краскала
     def GenKruskal(self, maze):
 
         w = maze.GetWidth()
         h = maze.GetHeight()
         n = (w - 1) * h + w * (h - 1)
-        
+
         Walls = [Wall() for x in range(n)]
 
-        #изначально все стены существуют
+        # изначально все стены существуют
         for i in range(w):
             for j in range(h):
                 maze.SetLoc(i, j, True, True)
 
         counter = 0;
-        for i in range(1,w):
+        for i in range(1, w):
             for j in range(h):
                 Walls[counter].x = i
                 Walls[counter].y = j
@@ -192,9 +294,9 @@ class GenMaze:
                 counter += 1
 
         tempw = Wall();
-        for k in range(10*n):
-            i = randint(0,n-1)
-            j = randint(0,n-1)
+        for k in range(10 * n):
+            i = randint(0, n - 1)
+            j = randint(0, n - 1)
             tempw = Walls[i]
             Walls[i] = Walls[j]
             Walls[j] = tempw
@@ -204,8 +306,71 @@ class GenMaze:
         while (locations > 1):
             tempw = Walls[ii]
             ii += 1
-            if (self.WaveTracingSolve(maze, tempw.x, tempw.y, tempw.x + tempw.dx, tempw.y + tempw.dy)==False):
+            if (self.WaveTracingSolve(maze, tempw.x, tempw.y, tempw.x + tempw.dx, tempw.y + tempw.dy) == False):
                 self.BreakWall(maze, tempw.x, tempw.y, tempw.dx, tempw.dy)
+                locations -= 1
+
+        maze.PrintToConsole();
+
+    # пошаговая генерация лабиринта методом Краскала
+    def GenKruskalSS(self, maze):
+
+        All = False;
+        Dlg = "";
+        w = maze.GetWidth()
+        h = maze.GetHeight()
+        n = (w - 1) * h + w * (h - 1)
+
+        Walls = [Wall() for x in range(n)]
+
+        # изначально все стены существуют
+        for i in range(w):
+            for j in range(h):
+                maze.SetLoc(i, j, True, True)
+
+        counter = 0;
+        for i in range(1, w):
+            for j in range(h):
+                Walls[counter].x = i
+                Walls[counter].y = j
+                Walls[counter].dx = -1
+                Walls[counter].dy = 0
+                counter += 1
+
+        for i in range(w):
+            for j in range(1, h):
+                Walls[counter].x = i
+                Walls[counter].y = j
+                Walls[counter].dx = 0
+                Walls[counter].dy = -1
+                counter += 1
+
+        tempw = Wall();
+        for k in range(10 * n):
+            i = randint(0, n - 1)
+            j = randint(0, n - 1)
+            tempw = Walls[i]
+            Walls[i] = Walls[j]
+            Walls[j] = tempw
+
+        locations = w * h
+        ii = 0
+        while (locations > 1):
+            tempw = Walls[ii]
+            ii += 1
+            if (self.WaveTracingSolve(maze, tempw.x, tempw.y, tempw.x + tempw.dx, tempw.y + tempw.dy) == False):
+                self.BreakWall(maze, tempw.x, tempw.y, tempw.dx, tempw.dy)
+                if (All == False):
+                    maze.PrintToConsole()
+                    while (True):
+                        print("Нажмите <Enter> для перехода к следующему шагу генерации;")
+                        print("Введите 'q' - если хотите пропустить оставшиеся промежуточные ходы пошаговой генерации")
+                        Dlg = input()
+                        if (Dlg == ""):
+                            break
+                        if (Dlg == "q"):
+                            All = True
+                            break
                 locations -= 1
 
         maze.PrintToConsole();
--- maze.py
+++ maze.py
@@ -1,64 +1,66 @@
 # -*- coding: utf-8 -*-
 import codecs
 
-#класс локации лабиринта
+
+# класс локации лабиринта
 class Location(object):
     def __init__(self, lw, uw):
         self.left_wall = lw
         self.up_wall = uw;
 
-#класс лабиринта
-class Maze(object):
-    #конструктор по умолчанию
-    def __init__(self):
-        #высота лабиринта
-        self.Height = 10
-        #ширина лабиринта
-        self.Width = 15
-        #локации лабиринта
-        self.Locs = [[Location for x in range(self.Height+1)]
-                     for y in range(self.Width+1)]
 
-    #вспомогательный метод, анализирующий наличие заданной комбинации
-    #стен у левого верхнего угла локации
+# класс лабиринта
+class Maze(object):
+    # конструктор по умолчанию
+    def __init__(self):
+        # высота лабиринта
+        self.Height = 10
+        # ширина лабиринта
+        self.Width = 15
+        # локации лабиринта
+        self.Locs = [[Location for x in range(self.Height + 1)]
+                     for y in range(self.Width + 1)]
+
+    # вспомогательный метод, анализирующий наличие заданной комбинации
+    # стен у левого верхнего угла локации
     def IsCombWalls(self, f1, f2, f3, f4, i, j):
-        if   (self.Locs[j-1][i  ].up_wall   == f1) and \
-             (self.Locs[j  ][i-1].left_wall == f2) and \
-             (self.Locs[j  ][i  ].up_wall   == f3) and \
-             (self.Locs[j  ][i  ].left_wall == f4):
+        if (self.Locs[j - 1][i].up_wall == f1) and \
+                (self.Locs[j][i - 1].left_wall == f2) and \
+                (self.Locs[j][i].up_wall == f3) and \
+                (self.Locs[j][i].left_wall == f4):
             return True
         else:
             return False
 
-    #метод, генерирующий набор строк с текстовым представлением
-    #лабиринта
+    # метод, генерирующий набор строк с текстовым представлением
+    # лабиринта
     def GetLinesMaze(self):
-        g = [ "\u2500\u2500\u2500\u2500", "    " ]
-        v = [ "\u2502   ", "\u2502   " ]
-            
-        lt = [ "\u250c\u2500\u2500\u2500", "\u2502   "     ]
-        lc = [ "\u251c\u2500\u2500\u2500", "\u2502   "     ]
-        lb = [ "\u2514\u2500\u2500\u2500", "    "]
+        g = ["\u2500\u2500\u2500\u2500", "    "]
+        v = ["\u2502   ", "\u2502   "]
 
-        ct = [ "\u252c\u2500\u2500\u2500", "\u2502   "]
-        cc = [ "\u253c\u2500\u2500\u2500", "\u2502   "]
-        cb = [ "\u2534\u2500\u2500\u2500", "    "]
+        lt = ["\u250c\u2500\u2500\u2500", "\u2502   "]
+        lc = ["\u251c\u2500\u2500\u2500", "\u2502   "]
+        lb = ["\u2514\u2500\u2500\u2500", "    "]
 
-        rt = [ "\u2510   ", "\u2502   "]
-        rc = [ "\u2524   ", "\u2502   "]
-        rb = [ "\u2518   ", "    "]
+        ct = ["\u252c\u2500\u2500\u2500", "\u2502   "]
+        cc = ["\u253c\u2500\u2500\u2500", "\u2502   "]
+        cb = ["\u2534\u2500\u2500\u2500", "    "]
 
-        #создаем массив представления (на экране консоли) локаций лабиринта
-        #для каждой локации - две строчки и еще дополнительные строка и
-        #столбец для нижней и правой границ лабиринта
-        c = [["    " for x in range(self.Width+1)]
-             for y in range((self.Height+1)*2)]
+        rt = ["\u2510   ", "\u2502   "]
+        rc = ["\u2524   ", "\u2502   "]
+        rb = ["\u2518   ", "    "]
 
-        #левый верхний угол лабиринта
+        # создаем массив представления (на экране консоли) локаций лабиринта
+        # для каждой локации - две строчки и еще дополнительные строка и
+        # столбец для нижней и правой границ лабиринта
+        c = [["    " for x in range(self.Width + 1)]
+             for y in range((self.Height + 1) * 2)]
+
+        # левый верхний угол лабиринта
         c[0][0] = lt[0]
         c[1][0] = lt[1]
 
-        #верхняя строка лабиринта
+        # верхняя строка лабиринта
         for j in range(1, self.Width):
             if (self.Locs[j][0].left_wall == True):
                 c[0][j] = ct[0]
@@ -66,93 +68,105 @@ class Maze(object):
             else:
                 c[0][j] = g[0]
                 c[1][j] = g[1]
-        
-        #правый верхний угол лабиринта
+
+        # правый верхний угол лабиринта
         c[0][self.Width] = rt[0]
         c[1][self.Width] = rt[1]
 
-        #крайний левый столбец лабиринта
+        # крайний левый столбец лабиринта
         for i in range(1, self.Height):
-             if (self.Locs[0][i].up_wall == True):
-                 c[2*i    ][0] = lc[0]
-                 c[2*i + 1][0] = lc[1]
-             else:
-                 c[2*i    ][0] = v[0]
-                 c[2*i + 1][0] = v[1]
-        
-        #левый нижний угол лабиринта
-        c[2*self.Height    ][0] = lb[0]
-        c[2*self.Height + 1][0] = lb[1]
+            if (self.Locs[0][i].up_wall == True):
+                c[2 * i][0] = lc[0]
+                c[2 * i + 1][0] = lc[1]
+            else:
+                c[2 * i][0] = v[0]
+                c[2 * i + 1][0] = v[1]
 
-        #вывод основных локаций лабиринта
+        # левый нижний угол лабиринта
+        c[2 * self.Height][0] = lb[0]
+        c[2 * self.Height + 1][0] = lb[1]
+
+        # вывод основных локаций лабиринта
         for i in range(1, self.Height):
             for j in range(1, self.Width):
-                if   self.IsCombWalls(False, False, False, True,  i, j) == True:
-                    c[2*i ][ j] = v[0];   c[2*i + 1][j] = v[1]
-                elif self.IsCombWalls(False, False, True,  False, i, j) == True:
-                    c[2*i ][ j] = g[0];   c[2*i + 1][ j] = g[1]
-                elif self.IsCombWalls(False, False, True,  True,  i, j) == True:
-                    c[2*i ][ j] = lt[0];  c[2*i + 1][ j] = lt[1]
-                elif self.IsCombWalls(True,  True,  False, False, i, j) == True:
-                    c[2*i ][ j] = rb[0];  c[2*i + 1][ j] = rb[1]
-                elif self.IsCombWalls(True,  False, False, True,  i, j) == True:
-                    c[2*i ][ j] = rt[0];  c[2*i + 1][ j] = rt[1]
-                elif self.IsCombWalls(True,  False, True,  False, i, j) == True:
-                    c[2*i ][ j] = g[0];   c[2*i + 1][ j] = g[1]
-                elif self.IsCombWalls(False, True,  True,  False, i, j) == True:
-                    c[2*i ][ j] = lb[0];  c[2*i + 1][ j] = lb[1]
-                elif self.IsCombWalls(False, True,  False, True,  i, j) == True:
-                    c[2*i ][ j] = v[0];   c[2*i + 1][ j] = v[1]
-                elif self.IsCombWalls(False, True,  True,  True,  i, j) == True:
-                    c[2*i ][ j] = lc[0];  c[2*i + 1][ j] = lc[1]
-                elif self.IsCombWalls(True,  False, True,  True,  i, j) == True:
-                    c[2*i ][ j] = ct[0];  c[2*i + 1][ j] = ct[1]
-                elif self.IsCombWalls(True,  True,  False, True,  i, j) == True:
-                    c[2*i ][ j] = rc[0];  c[2*i + 1][ j] = rc[1]
-                elif self.IsCombWalls(True,  True,  True,  False, i, j) == True:
-                    c[2*i ][j] = cb[0];   c[2*i + 1][ j] = cb[1]
-                elif self.IsCombWalls(True,  True,  True,  True,  i, j) == True:
-                    c[2*i ][ j] = cc[0];  c[2*i + 1][ j] = cc[1]
+                if self.IsCombWalls(False, False, False, True, i, j) == True:
+                    c[2 * i][j] = v[0];
+                    c[2 * i + 1][j] = v[1]
+                elif self.IsCombWalls(False, False, True, False, i, j) == True:
+                    c[2 * i][j] = g[0];
+                    c[2 * i + 1][j] = g[1]
+                elif self.IsCombWalls(False, False, True, True, i, j) == True:
+                    c[2 * i][j] = lt[0];
+                    c[2 * i + 1][j] = lt[1]
+                elif self.IsCombWalls(True, True, False, False, i, j) == True:
+                    c[2 * i][j] = rb[0];
+                    c[2 * i + 1][j] = rb[1]
+                elif self.IsCombWalls(True, False, False, True, i, j) == True:
+                    c[2 * i][j] = rt[0];
+                    c[2 * i + 1][j] = rt[1]
+                elif self.IsCombWalls(True, False, True, False, i, j) == True:
+                    c[2 * i][j] = g[0];
+                    c[2 * i + 1][j] = g[1]
+                elif self.IsCombWalls(False, True, True, False, i, j) == True:
+                    c[2 * i][j] = lb[0];
+                    c[2 * i + 1][j] = lb[1]
+                elif self.IsCombWalls(False, True, False, True, i, j) == True:
+                    c[2 * i][j] = v[0];
+                    c[2 * i + 1][j] = v[1]
+                elif self.IsCombWalls(False, True, True, True, i, j) == True:
+                    c[2 * i][j] = lc[0];
+                    c[2 * i + 1][j] = lc[1]
+                elif self.IsCombWalls(True, False, True, True, i, j) == True:
+                    c[2 * i][j] = ct[0];
+                    c[2 * i + 1][j] = ct[1]
+                elif self.IsCombWalls(True, True, False, True, i, j) == True:
+                    c[2 * i][j] = rc[0];
+                    c[2 * i + 1][j] = rc[1]
+                elif self.IsCombWalls(True, True, True, False, i, j) == True:
+                    c[2 * i][j] = cb[0];
+                    c[2 * i + 1][j] = cb[1]
+                elif self.IsCombWalls(True, True, True, True, i, j) == True:
+                    c[2 * i][j] = cc[0];
+                    c[2 * i + 1][j] = cc[1]
 
-
-        #вывод нижней строки лабиринта
+        # вывод нижней строки лабиринта
         for i in range(1, self.Width):
             if (self.Locs[i][self.Height - 1].left_wall == True):
-                c[2*self.Height    ][i] = cb[0]
-                c[2*self.Height + 1][i] = cb[1]
+                c[2 * self.Height][i] = cb[0]
+                c[2 * self.Height + 1][i] = cb[1]
             else:
-                c[2*self.Height    ][i] = g[0]
-                c[2*self.Height + 1][i] = g[1]
+                c[2 * self.Height][i] = g[0]
+                c[2 * self.Height + 1][i] = g[1]
 
-        #вывод крайнего правого столбца лабиринта
-        for i in range (1, self.Height):
+        # вывод крайнего правого столбца лабиринта
+        for i in range(1, self.Height):
             if (self.Locs[self.Width - 1][i].up_wall == True):
-                c[2*i    ][ self.Width] = rc[0]
-                c[2*i + 1][ self.Width] = rc[1]
+                c[2 * i][self.Width] = rc[0]
+                c[2 * i + 1][self.Width] = rc[1]
             else:
-                c[2*i    ][ self.Width] = v[0]
-                c[2*i + 1][ self.Width] = v[1]
+                c[2 * i][self.Width] = v[0]
+                c[2 * i + 1][self.Width] = v[1]
 
-        #правый нижний угол лабиринта
-        c[2*self.Height    ][ self.Width] = rb[0]
-        c[2*self.Height + 1][ self.Width] = rb[1]
-        
-        #формирование строк лабиринта
-        s = ["" for x in range((self.Height+1)*2)]
-        for i in range(self.Height+1):
-            for j in range(self.Width+1):
-                s[2*i    ] += c[2*i    ][ j]
-                s[2*i + 1] += c[2*i + 1][ j]
+        # правый нижний угол лабиринта
+        c[2 * self.Height][self.Width] = rb[0]
+        c[2 * self.Height + 1][self.Width] = rb[1]
+
+        # формирование строк лабиринта
+        s = ["" for x in range((self.Height + 1) * 2)]
+        for i in range(self.Height + 1):
+            for j in range(self.Width + 1):
+                s[2 * i] += c[2 * i][j]
+                s[2 * i + 1] += c[2 * i + 1][j]
 
         return s
 
-    #напечатать лабиринт
+    # напечатать лабиринт
     def PrintToConsole(self):
         s = self.GetLinesMaze()
-        
-        for i in range(self.Height+1):
-            print(s[2*i])
-            print(s[2*i+1])
+
+        for i in range(self.Height + 1):
+            print(s[2 * i])
+            print(s[2 * i + 1])
 
     def GetLoc(self, i, j):
         return self.Locs[i][j];
@@ -177,8 +191,8 @@ class Maze(object):
         self.Width = w
         self.Height = h
 
-        self.Locs = [[Location(False, False) for x in range(self.Height+1)]
-                     for y in range(self.Width+1)]
+        self.Locs = [[Location(False, False) for x in range(self.Height + 1)]
+                     for y in range(self.Width + 1)]
 
         for i in range(self.Width + 1):
             self.Locs[i][self.Height] = Location(False, True);
